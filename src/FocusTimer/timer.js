c6f924c2ace024a5963bf99e534b1cadc0d84e2d
Humberto Galdino <humberto.galdino@live.com>
2023-12-31 15:43:12 -0300

    Adicionando sounds

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,8 +1,11 @@
 import state from "./state.js";
 import * as elements from "./elements.js";
 import { reset } from "./actions.js";
+import { kitchenTimer } from "./sounds.js";
 
 export function countdown(){
+    clearTimeout(state.countdownId);
+
     if(!state.isRunning){
         return
     }
@@ -19,13 +22,14 @@ export function countdown(){
 
     if(minutes < 0){
         reset();
+        kitchenTimer.play();
         return;
     }
 
     updateDisplay(minutes, seconds);
 
     // recursão -> uma função chamando ela mesma
-    setTimeout(() => countdown(), 1000);
+    state.countdownId = setTimeout(() => countdown(), 1000);
 }
 
 export function updateDisplay(minutes, seconds) {
