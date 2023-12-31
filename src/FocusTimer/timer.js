00920f2c1da80039f7ecba3056811e169eed2cc4
Humberto Galdino <humberto.galdino@live.com>
2023-12-30 21:19:14 -0300

    Criando a lógica do countdown

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,5 +1,32 @@
 import state from "./state.js";
 import * as elements from "./elements.js";
+import { reset } from "./actions.js";
+
+export function countdown(){
+    if(!state.isRunning){
+        return
+    }
+
+    let minutes = Number(elements.minutes.textContent);
+    let seconds = Number(elements.seconds.textContent);
+
+    seconds--;
+    
+    if(seconds < 0){
+        seconds = 59;
+        minutes--;
+    }
+
+    if(minutes < 0){
+        reset();
+        return;
+    }
+
+    updateDisplay(minutes, seconds);
+
+    // recursão -> uma função chamando ela mesma
+    setTimeout(() => countdown(), 1000);
+}
 
 export function updateDisplay(minutes, seconds) {
     minutes = minutes ?? state.minutes;
