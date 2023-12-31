cdc3f4b5601aaa90b51dbb48f8bf189fd8c9403f
Humberto Galdino <humberto.galdino@live.com>
2023-12-30 21:38:00 -0300

    Finalizando lógica de edição dos minutos do contador

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,6 +1,8 @@
+import state from "./state.js";
 import { controls } from "./elements.js";
 import * as actions from "./actions.js";
 import * as elements from "./elements.js"
+import { updateDisplay } from "./timer.js";
 
 export function registerControls() {
     controls.addEventListener('click', (event) => {
@@ -16,10 +18,23 @@ export function registerControls() {
 
 export function setMinutes(){
     elements.minutes.addEventListener('focus', () => {
-        elements.minutes.textContent = ""
-    })
+        elements.minutes.textContent = "";
+    });
 
     // embora esteja riscado por estar deprecated, a estratégia ainda funciona
     // o código abaixo permitirá que sejam digitados apenas números na aplicação
-    elements.minutes.onkeypress = (event) => /\d/.test(event.key)
+    elements.minutes.onkeypress = (event) => /\d/.test(event.key);
+
+    // ao sair do foco determine que no máximo serão apenas 60 minutos
+    elements.minutes.addEventListener('blur', (event) => {
+        let time = event.currentTarget.textContent;
+        time = time > 60 ? 60 : time;
+
+        state.minutes = time;
+        state.seconds = 0;
+
+        updateDisplay();
+        
+        elements.minutes.removeAttribute('contenteditable');
+    });
 }
